dd51e16779a73367a03e460a87d0c8474928cf7c
Veronika <veronika.novosad4@gmail.com>
2024-05-10 18:59:12 +0200

    Done

---
+++ WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,31 @@
+using WebApplication1.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+//dependency
+builder.Services.AddScoped<IBooksRepository, BooksRepository>();
+builder.Services.AddScoped<IGenresRepository, GenresRepository>();
+//builder.Services.AddScoped<IWarehouses2Repository, Warehouses2Repository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
